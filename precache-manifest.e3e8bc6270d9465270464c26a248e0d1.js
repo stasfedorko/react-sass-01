60c5b8f5cea8a078f74470cc3bdaf28f5693aca8
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-07-04 00:40:39 +0300

    Updates

---
+++ precache-manifest.e3e8bc6270d9465270464c26a248e0d1.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba7111de436bb1354616d380f6c747a",
+    "url": "/react-sass-01/index.html"
+  },
+  {
+    "revision": "4ac01439f30ee78bb801",
+    "url": "/react-sass-01/static/css/10.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "90944bf4e5047910034e",
+    "url": "/react-sass-01/static/css/7.4707e12a.chunk.css"
+  },
+  {
+    "revision": "a8e2ab86f359f69b1878",
+    "url": "/react-sass-01/static/css/9.579c8824.chunk.css"
+  },
+  {
+    "revision": "1fac7ac2271b790a1ed7",
+    "url": "/react-sass-01/static/js/0.788078a8.chunk.js"
+  },
+  {
+    "revision": "64c82428a8098ed7432f",
+    "url": "/react-sass-01/static/js/1.f599d03e.chunk.js"
+  },
+  {
+    "revision": "259406de3fa45c939f708b51a5a622de",
+    "url": "/react-sass-01/static/js/1.f599d03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac01439f30ee78bb801",
+    "url": "/react-sass-01/static/js/10.187b3c9e.chunk.js"
+  },
+  {
+    "revision": "55836ab3d86a2bd64c4a",
+    "url": "/react-sass-01/static/js/11.ec23cd94.chunk.js"
+  },
+  {
+    "revision": "804509236d09c8a285d1",
+    "url": "/react-sass-01/static/js/12.362d1fa4.chunk.js"
+  },
+  {
+    "revision": "72e984d0a87bc90f58a2",
+    "url": "/react-sass-01/static/js/13.51a730cd.chunk.js"
+  },
+  {
+    "revision": "304e42245498aaf445a0",
+    "url": "/react-sass-01/static/js/14.bf243568.chunk.js"
+  },
+  {
+    "revision": "7d9a096e17b23bc663b9",
+    "url": "/react-sass-01/static/js/15.6e525b7c.chunk.js"
+  },
+  {
+    "revision": "387b689075df873a319d",
+    "url": "/react-sass-01/static/js/16.22bc8ec2.chunk.js"
+  },
+  {
+    "revision": "19cb74e23cfd6b6999a6",
+    "url": "/react-sass-01/static/js/17.8c6a3b19.chunk.js"
+  },
+  {
+    "revision": "95c860f7c5114b781b46abadb3ee6e02",
+    "url": "/react-sass-01/static/js/17.8c6a3b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940fe5384b45ee90b31a",
+    "url": "/react-sass-01/static/js/18.8e78f19b.chunk.js"
+  },
+  {
+    "revision": "84d4e0f5de274bc36ff0",
+    "url": "/react-sass-01/static/js/19.77566fde.chunk.js"
+  },
+  {
+    "revision": "7de9371c222e18f5786d",
+    "url": "/react-sass-01/static/js/2.fc1e568e.chunk.js"
+  },
+  {
+    "revision": "c6f1faefdf406a84d627",
+    "url": "/react-sass-01/static/js/20.b264330c.chunk.js"
+  },
+  {
+    "revision": "68172d07a24b9bcf2f63",
+    "url": "/react-sass-01/static/js/5.176efe56.chunk.js"
+  },
+  {
+    "revision": "f01b6ee26155721017cc5fc8eb2b8875",
+    "url": "/react-sass-01/static/js/5.176efe56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be1a83ef60548ab0781",
+    "url": "/react-sass-01/static/js/6.627b2d1f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-sass-01/static/js/6.627b2d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90944bf4e5047910034e",
+    "url": "/react-sass-01/static/js/7.cb40658f.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/react-sass-01/static/js/7.cb40658f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155eca577e10b80baeeb",
+    "url": "/react-sass-01/static/js/8.37bb0f9f.chunk.js"
+  },
+  {
+    "revision": "a8e2ab86f359f69b1878",
+    "url": "/react-sass-01/static/js/9.dc824c59.chunk.js"
+  },
+  {
+    "revision": "081234c9e91b96667cf5",
+    "url": "/react-sass-01/static/js/main.f614fda9.chunk.js"
+  },
+  {
+    "revision": "afe68ac06c48d95595975c9c465c41f8",
+    "url": "/react-sass-01/static/js/main.f614fda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34aa2f311f37c940c20",
+    "url": "/react-sass-01/static/js/runtime-main.9616fe6c.js"
+  },
+  {
+    "revision": "2b466f14430eac13337c9452ff05635c",
+    "url": "/react-sass-01/static/media/blogPost1.2b466f14.jpg"
+  },
+  {
+    "revision": "f574380de5938344b4dafe04d833270f",
+    "url": "/react-sass-01/static/media/blogPost2.f574380d.jpg"
+  },
+  {
+    "revision": "8d115020e50d1cdeeb160b7a2f3a88fb",
+    "url": "/react-sass-01/static/media/blogPost3.8d115020.jpg"
+  },
+  {
+    "revision": "29e641ea808c1389ddc7973e7e6b37c8",
+    "url": "/react-sass-01/static/media/blogPost4.29e641ea.jpg"
+  },
+  {
+    "revision": "eed51f556c7b96ada363087472035509",
+    "url": "/react-sass-01/static/media/blogPost5.eed51f55.jpg"
+  },
+  {
+    "revision": "070efa17c85068a75cf78b4e6874a570",
+    "url": "/react-sass-01/static/media/blogPost6.070efa17.jpg"
+  },
+  {
+    "revision": "99204534af09d684ae68e4b7a9b7c1c7",
+    "url": "/react-sass-01/static/media/feedback-01.99204534.png"
+  },
+  {
+    "revision": "18a466513ac0657a22656a7ac7c53bc0",
+    "url": "/react-sass-01/static/media/feedback-02.18a46651.png"
+  },
+  {
+    "revision": "816636e1f7ebe84d6da83e042d37ba5a",
+    "url": "/react-sass-01/static/media/feedback-03.816636e1.png"
+  },
+  {
+    "revision": "ce64c0a6aefd3b9d61736f230712907b",
+    "url": "/react-sass-01/static/media/feedback-04.ce64c0a6.png"
+  },
+  {
+    "revision": "9af4b2895252b1c88acc9bd8d7377138",
+    "url": "/react-sass-01/static/media/feedback-05.9af4b289.png"
+  },
+  {
+    "revision": "7a724613c977bbc156f14f1e05a8f54f",
+    "url": "/react-sass-01/static/media/headerImage.7a724613.jpg"
+  },
+  {
+    "revision": "e69e6026a423d9ec529b57455907b2b1",
+    "url": "/react-sass-01/static/media/image1.e69e6026.jpg"
+  },
+  {
+    "revision": "8bb0c1c3f829049e059c5af0d0417f64",
+    "url": "/react-sass-01/static/media/image10.8bb0c1c3.jpg"
+  },
+  {
+    "revision": "bda67d264e2b4b06614c31670c6a8948",
+    "url": "/react-sass-01/static/media/image3.bda67d26.jpg"
+  },
+  {
+    "revision": "79ebeb1fee81de72e553c9b30632711e",
+    "url": "/react-sass-01/static/media/image4.79ebeb1f.jpg"
+  },
+  {
+    "revision": "39a17f69e21b46e0cecbf738282960b6",
+    "url": "/react-sass-01/static/media/image5.39a17f69.jpg"
+  },
+  {
+    "revision": "8b868e181cca8220ca5e725a1f7b984d",
+    "url": "/react-sass-01/static/media/image6.8b868e18.jpg"
+  },
+  {
+    "revision": "e052cbd6b2db7efc7de674d3047c998c",
+    "url": "/react-sass-01/static/media/image7.e052cbd6.jpg"
+  },
+  {
+    "revision": "b23293625f3b70e68020633ce1427566",
+    "url": "/react-sass-01/static/media/image8.b2329362.jpg"
+  },
+  {
+    "revision": "b43804d383d9ba29df937e93cca4f1c1",
+    "url": "/react-sass-01/static/media/image9.b43804d3.jpg"
+  }
+]);
